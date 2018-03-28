4224a69f23ba046ea7f74e913c4ae5d5118a211d
naved001 <naved001@gmail.com>
2018-03-28 13:04:42 -0400

    Delete this committ

--- ci/obmd/run_integration_tests.sh
+++ ci/obmd/run_integration_tests.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 set -ex
-export PATH=$PATH:/usr/local/go/bin
+export PATH=/usr/local/go/bin:$PATH
+which go
+go version
+exit 2
 py.test --cov=hil --cov-append tests/integration/migrate_ipmi_info.py
