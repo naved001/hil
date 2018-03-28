0d2463d3a2048c1ce9919e7fc774f94d99b35a2e
naved001 <naved001@gmail.com>
2018-03-28 09:54:14 -0400

    Add file to install Go manually rather than getting it from apt because that downloads version an older version.

---
+++ ci/install_go.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+set -ex
+
+wget https://dl.google.com/go/go1.10.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.10.linux-amd64.tar.gz
--- ci/obmd/run_integration_tests.sh
+++ ci/obmd/run_integration_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -ex
-
+export PATH=$PATH:/usr/local/go/bin
 py.test --cov=hil --cov-append tests/integration/migrate_ipmi_info.py
