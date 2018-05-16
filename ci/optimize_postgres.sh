85693976dea79321f994220258e63cad0c335897
Naved Ansari <naved001@gmail.com>
2018-05-16 14:42:18 -0400

    Use postgres 10

--- ci/optimize_postgres.sh
+++ ci/optimize_postgres.sh
@@ -6,6 +6,10 @@ if [ $DB = sqlite ]; then
     exit 0
 fi
 
+sudo apt install tree -y
+cd /etc/postgresql
+tree
+
 # these changes optimize postgres. They are fine for testing, but not suitable
 # for production.
 # See https://www.postgresql.org/docs/current/static/non-durability.html
@@ -13,6 +17,6 @@ echo "fsync = off
 synchronous_commit = off
 full_page_writes = off
 checkpoint_timeout = 30min
-"| sudo tee --append /etc/postgresql/9.*/main/postgresql.conf
+"| sudo tee --append /etc/postgresql/*/main/postgresql.conf
 
 sudo service postgresql restart
